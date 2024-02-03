3b4769d8795f7e54ee033a4cd53d9c853c9572d2
shawon1fb <shawon0187@gmail.com>
2024-02-03 13:36:24 +0600

    code rearrange

--- Sources/EasyX/EasyFiles/EasyFile.swift
+++ Sources/EasyX/EasyFiles/EasyFile.swift
@@ -13,21 +13,21 @@ public enum FileError: Error {
 public enum FileMode {
     /// The mode for opening a file only for reading.
     case read
-
+    
     /// Mode for opening a file for reading and writing. The file is
     /// overwritten if it already exists. The file is created if it does not
     /// already exist.
     case write
-
+    
     /// Mode for opening a file for reading and writing to the
     /// end of it. The file is created if it does not already exist.
     case append
-
+    
     /// Mode for opening a file for writing *only*. The file is
     /// overwritten if it already exists. The file is created if it does not
     /// already exist.
     case writeOnly
-
+    
     /// Mode for opening a file for writing *only* to the
     /// end of it. The file is created if it does not already exist.
     case writeOnlyAppend
@@ -40,29 +40,28 @@ public protocol IFile{
     func getPath() -> String
     func writeAsBytes(_ bytes: Data, mode: FileMode  )  throws -> IFile
     func writeAsString(_ string: String, mode: FileMode )  throws -> IFile
-    
     func createSync( recursive:Bool,  exclusive:Bool ) throws
 }
 
 public final class File: IFile, Sendable {
     public  func createSync(recursive: Bool, exclusive: Bool) throws {
         if FileManager.default.fileExists(atPath: url.path) {
-              if exclusive {
-                  throw FileError.fileAlreadyExists
-              } else {
-                  return // File already exists, no action needed
-              }
-          }
-          // Create intermediate directories if necessary
-          if recursive {
-              try FileManager.default.createDirectory(at: url.deletingLastPathComponent(), withIntermediateDirectories: true)
-          }
-
-          // Create the file itself
-          FileManager.default.createFile(atPath: url.path, contents: nil)
+            if exclusive {
+                throw FileError.fileAlreadyExists
+            } else {
+                return // File already exists, no action needed
+            }
+        }
+        // Create intermediate directories if necessary
+        if recursive {
+            try FileManager.default.createDirectory(at: url.deletingLastPathComponent(), withIntermediateDirectories: true)
+        }
+        
+        // Create the file itself
+        FileManager.default.createFile(atPath: url.path, contents: nil)
     }
     
-   
+    
     
     private let url: URL
     
@@ -70,11 +69,11 @@ public final class File: IFile, Sendable {
         self.url = url
     }
     
-
+    
     public func exists() -> Bool {
         return FileManager.default.fileExists(atPath: url.path)
     }
-
+    
     public func readAsBytes() throws -> Data {
         do {
             return try Data(contentsOf: url)
@@ -82,7 +81,7 @@ public final class File: IFile, Sendable {
             throw FileError.readingFailed
         }
     }
-
+    
     public  func readAsString() throws -> String {
         do {
             return try String(contentsOf: url, encoding: .utf8)
@@ -90,16 +89,16 @@ public final class File: IFile, Sendable {
             throw FileError.readingFailed
         }
     }
-
+    
     public func getPath() -> String {
         return url.path
     }
-
+    
     public func writeAsBytes(_ bytes: Data, mode: FileMode = .write) throws -> IFile {
         do {
             
             if exists() == false{
-              try createSync(recursive: true, exclusive: false)
+                try createSync(recursive: true, exclusive: false)
             }
             
             switch mode {
@@ -120,7 +119,7 @@ public final class File: IFile, Sendable {
             throw FileError.encodingFailed
         }
     }
-
+    
     public func writeAsString(_ string: String, mode: FileMode = .write) throws -> IFile {
         guard let data = string.data(using: .utf8) else {
             throw FileError.encodingFailed
